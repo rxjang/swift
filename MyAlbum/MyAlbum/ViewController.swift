34c99a15f11a771e6b8d7d1764fd7c2e52485c94
rxjang <rxforp@naver.com>
2020-12-04 11:34:17 +0900

    view Controller

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by Seohyun Jang on 2020/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func hello(_ sender: Any) {
+        print("hello")
+    }
+    
+
+}
+
