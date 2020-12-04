d0d239c30903efd84c8fa60f0886792fd1ca3be0
rxjang <rxforp@naver.com>
2020-12-04 11:39:56 +0900

    alert

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -11,10 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     @IBAction func hello(_ sender: Any) {
-        print("hello")
+        let alert=UIAlertController(title: "Hello", message: "My First App!!", preferredStyle: .alert)
+        let action=UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 
